e1076eb4cb14abc7385e2220ab43e4dc3a133a9c
Dipanjan Kailthya <dipanjan.kailthya@gmail.com>
2026-09-22 09:05:22 +0200

    Delete ALB and ECS

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,44 +1,44 @@
-resource "aws_security_group" "alb" {
-  name        = "${local.project_name}-alb-sg"
-  description = "Allow inbound HTTP/HTTPS to the ALB"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = local.tags
-}
-
-resource "aws_lb" "main" {
-  name               = "${local.project_name}-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb.id]
-  subnets            = module.vpc.public_subnets
-
-  tags = local.tags
-}
-
+# resource "aws_security_group" "alb" {
+#   name        = "${local.project_name}-alb-sg"
+#   description = "Allow inbound HTTP/HTTPS to the ALB"
+#   vpc_id      = module.vpc.vpc_id
+#
+#   ingress {
+#     description = "HTTP"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   ingress {
+#     description = "HTTPS"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#
+#   tags = local.tags
+# }
+#
+# resource "aws_lb" "main" {
+#   name               = "${local.project_name}-alb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.alb.id]
+#   subnets            = module.vpc.public_subnets
+#
+#   tags = local.tags
+# }
+#
 # resource "aws_lb_listener" "http" {
 #   load_balancer_arn = aws_lb.main.arn
 #   port              = 80
